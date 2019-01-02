2183ecb5d3ffb78f1277f133e94ec3103c71036e
Soni <sonis@vwcredit.com>
2019-01-02 17:30:54 -0600

    adding car tile

--- src/App.js
+++ src/App.js
@@ -1,55 +1,100 @@
 import React, { Component } from 'react';
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import logo from './logo.svg';
 import './App.css';
 
 //get the data from graphQl API
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
-import { reduce } from 'async';
+let year = 1;
 
 class App extends Component {
+
+  constructor () {
+    super()
+    this.state = {
+      year: 0
+    }
+    this.showyearlyData = this.showyearlyData.bind(this)
+    }
+
+    showyearlyData (yr) {
+    console.log('showyearlyData', yr);
+    this.setState({ year : yr})
+    }
+
   render() {
     const client = new ApolloClient({
-      uri: "https://api.graph.cool/simple/v1/ciyz901en4j590185wkmexyex"
+      uri: "/vehicle/graphql/",
+      fetchOptions: {
+        mode: 'no-cors'
+    }
     });
 
     const userTile = {
       color: '#333',
       padding: '10px',
-      margin: '10px',
+      marginBottom: '10px',
       border: '1px solid'
     }
+    const header = {
+      padding: '10px',
+    }
+
+
+
     return (
       <ApolloProvider client={client}>
+      <div style={header}>
+        <h2>REACT GRAPHQL Apollo sample app</h2>
       <div>
-        <h2>Employee Info Apollo app</h2>
-      </div>
+      &nbsp;<button onClick={() => this.showyearlyData(0)} type="button" class="btn btn-lg btn-primary">2019</button> &nbsp;
+      <button onClick={() => this.showyearlyData(1)} type="button" class="btn btn-lg btn-default">2018</button>&nbsp;
+      <button onClick={() => this.showyearlyData(2)} type="button" class="btn btn-lg btn-default">2017</button>
 
-       <Query
+    </div>
+    </div>
+
+    <Query
     query={gql`
-      {
-        allUsers {
-          id
-          name
+    {
+      GetModelTrimsByYear(brand:VW, country:USA, modelSalesYears:["2019", "2018", "2019"]){
+        data{
+          salesYear,
+          modelTrims{
+            data{
+              modelName,
+              modelSalesYear,
+              media{
+                imageUrl
+              }
+            }
+          }
         }
       }
+    }
     `}
   >
     {({ loading, error, data }) => {
       if (loading) return <p>Loading...</p>;
       if (error) return <p>Error :(</p>;
-      return data.allUsers.map(({ id, name }) => (
-        <div key={id} style={userTile} >
-          <p><strong>Employee ID : </strong> {`${id}`}</p>
-          <p><strong>Emplyee Name : </strong> {` ${name}`}</p>
+      console.log('got the data', data.GetModelTrimsByYear.data[year].modelTrims.data);
+     return data.GetModelTrimsByYear.data[this.state.year].modelTrims.data.map(({ modelName, modelSalesYear, media }) => (
+       <div className="">
+        <div className="col-sm-3">
+        <div  style={userTile} >
+        <img width="200" src={` ${media.imageUrl}`} alt={modelName}/>
+          <p><strong>{` ${modelName} ${modelSalesYear}`}</strong> </p>
         </div>
+        </div>
+       </div>
+       
+        
       ));
     }}
   </Query>
     </ApolloProvider>
-      
+   
     );
   }
 }
